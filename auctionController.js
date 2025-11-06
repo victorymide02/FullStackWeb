9b01724dd7338d62d972fe5c9ef03da3e664d104
victorymide02 <victorymide02@outlook.com>
2025-11-06 12:07:31 +0000

    Add files via upload

---
+++ auctionController.js
@@ -0,0 +1,24 @@
+import * as model from "../models/auctionModel.js";
+import { auctionSchema } from "../validation/auctionValidation.js";
+
+export async function getAllAuctions(req, res, next) {
+    try {
+        const auctions = await model.getAll();
+        res.json(auctions);
+    } catch (err) {
+        next(err);
+    }
+}
+
+export async function createAuction(req, res, next) {
+    try {
+        const { error, value } = auctionSchema.validate(req.body);
+        if (error) {
+            return res.status(400).json({ message: error.details[0].message });
+        }
+        const newAuction = await model.create(value);
+        res.status(201).json(newAuction);
+    } catch (err) {
+        next(err);
+    }
+}
